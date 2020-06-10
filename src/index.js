8844953dc17569c76c541a941fa3c1e5b5cbade9
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2020-06-11 08:52:08 +0900

    [update] 6.4.2 ユーザー識別までおわり

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import App from './App'
+import { ApolloProvider } from 'react-apollo'
+import ApolloClient, { gql } from 'apollo-boost'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const client = new ApolloClient({ 
+  uri: 'http://localhost:4000/graphql', 
+  request: operation => {
+      operation.setContext(context => ({
+          headers: {
+              ...context.headers,
+              authorization: localStorage.getItem('token')
+          }
+      }))
+  }
+})
+
+render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>, 
+    document.getElementById('root')
+)
